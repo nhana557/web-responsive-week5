fad9f8a4511b9ed9c1421e068dc70b107dd0d8a8
Taryana <hatakekakasih2002@gmail.com>
2022-08-29 10:28:51 +0700

    added first

---
+++ assets/js/index.js
@@ -0,0 +1,66 @@
+const btn1 = document.querySelector('.btn1')
+const btn2 = document.querySelector('.btn2')
+const btn3 = document.querySelector('.btn3')
+const btn4 = document.querySelector('.btn4')
+const btn5 = document.querySelector('.btn5')
+const btn6 = document.getElementById('btn6')
+const btn7 = document.getElementById('btn7')
+const btn8 = document.getElementById('btn8')
+const btn9 = document.getElementById('btn9')
+const btn10 = document.getElementById('btn10')
+
+const color1 = document.querySelector('.B1')
+const color2 = document.querySelector('.B2')
+const color3 = document.querySelector('.B3')
+const color4 = document.querySelector('.B4')
+const color5 = document.querySelector('.B5')
+
+
+color1.addEventListener('click', function(){
+    color1.classList.toggle('border-active')
+})
+color2.addEventListener('click', function(){
+    color2.classList.toggle('border-active')
+})
+color3.addEventListener('click', function(){
+    color3.classList.toggle('border-active')
+})
+color4.addEventListener('click', function(){
+    color4.classList.toggle('border-active')
+})
+color5.addEventListener('click', function(){
+    color5.classList.toggle('border-active')
+})
+
+
+btn1.addEventListener('click', function(){
+    btn1.classList.toggle('bg-active')
+})
+btn2.addEventListener('click', function(){
+    btn2.classList.toggle('bg-active')
+})
+btn3.addEventListener('click', function(){
+    btn3.classList.toggle('bg-active')
+})
+btn4.addEventListener('click', function(){
+    btn4.classList.toggle('bg-active')
+})
+btn5.addEventListener('click', function(){
+    btn5.classList.toggle('bg-active')
+})
+btn6.addEventListener('click', function(){
+    btn6.classList.toggle('bg-active')
+})
+btn7.addEventListener('click', function(){
+    btn7.classList.toggle('bg-active')
+})
+btn8.addEventListener('click', function(){
+    btn8.classList.toggle('bg-active')
+})
+btn9.addEventListener('click', function(){
+    btn9.classList.toggle('bg-active')
+})
+btn10.addEventListener('click', function(){
+    btn10.classList.toggle('bg-active')
+})
+
